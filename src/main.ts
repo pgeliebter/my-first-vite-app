a116bb19d1a281212442ec38b3d75628d4f99140
Pinny Geliebter <pgibralter@gmail.com>
2021-09-03 09:52:39 -0400

    tries to get some JS working

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import './style.css'
 
 const app = document.querySelector<HTMLDivElement>('#app')!
+const ts = document.querySelector<HTMLDivElement>('#ts')!
+
+
 
 app.innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `
+
+ts.innerHTML=`
+<input id="nameInput" type=text placeholder="Enter your name"/>
+<h2>Hello <h1 id="boo"></h1> from typescript!</h2>
+<button id="submit" onclick="myFunction();">Submit</button>
+<script>`
+function myFunction(){
+  let name = "Pinny"
+  console.log(name)
+  const nameInput = document.querySelector("#nameInput");
+  console.log(nameInput)
+  if (nameInput) {name = nameInput.value} 
+  }`</script>
+`
+
